ad842db02cf011b02a9c579a5d496319c5d6ba4d
Izhar Abaasi <izharabbasi103@gmail.com>
2020-06-12 17:23:45 +0500

    add all child parsers

--- parsers/quotes.py
+++ parsers/quotes.py
@@ -1,7 +1,19 @@
-from locators.quotes_page_locators import PageQuotes
+from locators.quotes_locators import Quotes
+
 
 class QuotesParser:
-    def __init__(self,parent):
+    def __init__(self, parent):
         self.parent = parent
 
-        
+    def Content(self):
+        locator = Quotes.CONTENT
+        return self.parent.select_one(locator).string
+
+    def Author(self):
+        locator = Quotes.AUTHOR
+        return self.parent.select_one(locator).string
+
+    def tags(self):
+        locator = Quotes.TAGS
+        return [e.string for e in self.parent.select_one(locator)]
+
